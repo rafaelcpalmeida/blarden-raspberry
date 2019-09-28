93f17378410292cb98c346a1b87eb83531360d30
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2019-09-28 18:30:45 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  
+  "github.com/stianeikeland/go-rpio/v4"
+)
+
+func main() {
+  err := rpio.Open()
+  
+  if err != nil {
+    panic(fmt.Sprint("unable to open gpio", err.Error()))
+  }
+  
+  pin := rpio.Pin(24)
+  pin.Output()
+  
+  pin.High()
+  time.Sleep(1 * time.Second)
+  pin.Low()
+  
+  rpio.Close()
+}
