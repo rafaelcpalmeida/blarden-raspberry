b62cf0e808354cfb9cb8e9316297be2f16c08f21
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2019-09-29 11:42:47 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+    r := gin.Default()
+    r.GET("/open-door", func(c *gin.Context) {
+        OpenDoor()
+        c.JSON(200, gin.H{
+            "status": "open",
+        })
+    })
+    r.Run()
+}
