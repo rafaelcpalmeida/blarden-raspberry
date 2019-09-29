4a1521f71fee9617982586a0bcb50d1a7c0ab9ee
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2019-09-29 13:13:01 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import "github.com/gin-gonic/gin"
 
 func main() {
     r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H {
+            "ALIVE"
+        })
+    })
+
     r.GET("/open-door", func(c *gin.Context) {
         OpenDoor()
         c.JSON(200, gin.H{
             "status": "open",
         })
     })
+
     r.Run()
 }
