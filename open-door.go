703bc9fbd967c3d61811f50e5f0a30e79f63cee6
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2019-10-09 12:46:03 +0100

    Fix indentation

--- open-door.go
+++ open-door.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  
-  "github.com/stianeikeland/go-rpio/v4"
+    "fmt"
+    "time"
+
+    "github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
-  err := rpio.Open()
+    err := rpio.Open()
   
-  if err != nil {
-    panic(fmt.Sprint("unable to open gpio", err.Error()))
-  }
+    if err != nil {
+        panic(fmt.Sprint("unable to open gpio", err.Error()))
+    }
   
-  pin := rpio.Pin(24)
-  pin.Output()
+    pin := rpio.Pin(24)
+    pin.Output()
   
-  pin.High()
-  time.Sleep(1 * time.Second)
-  pin.Low()
+    pin.High()
+    time.Sleep(1 * time.Second)
+    pin.Low()
   
-  rpio.Close()
+    rpio.Close()
 }
