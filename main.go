cd046dd451c99360f122fbe9c2ef4e64360bca2e
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2019-10-12 23:04:26 +0100

    Update main.go

--- main.go
+++ main.go
@@ -107,6 +107,7 @@ func main() {
 	})
 
 	r.POST("/open-door", api2apiValidationMiddleware(), func(c *gin.Context) {
+		OpenDoor()
 		log.Info("I've opened the door")
 		c.JSON(200, gin.H{
 			"status": "open",
